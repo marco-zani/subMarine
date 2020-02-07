672148861c1eb7a7b4a12a97c594a7439d713571
Giulio <giulio.cappelli1e@gmail.com>
2020-02-07 19:33:33 +0100

    Bullet implementation
    TODO: eliminare il proiettile di default

---
+++ subMarine/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public static float speed;
+
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb2d.velocity = transform.up * speed;
+    }
+}
---
+++ subMarine/Assets/Scripts/ShootBullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+    public Rigidbody2D projectile;
+    public Transform Launcher;
+    public float speed = 2f;
+
+    private float distance = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Vector3 spawnPos = Launcher.transform.position + Launcher.transform.up * distance;
+
+            Rigidbody2D projectileInstance;
+            projectileInstance = Instantiate(projectile, spawnPos, Launcher.rotation) as Rigidbody2D;
+
+            BulletMovement.speed = speed;
+        }
+    }
+}
