d9c53cf70f1c5746ab37a635789166ee26020b11
Giulio <giulio.cappelli1e@gmail.com>
2020-02-07 21:56:26 +0100

    Final Bullet

--- subMarine/Assets/Scripts/ShootBullet.cs
+++ subMarine/Assets/Scripts/ShootBullet.cs
@@ -5,31 +5,33 @@ using UnityEngine;
 public class ShootBullet : MonoBehaviour
 {
     public Rigidbody2D projectile;
-    public Transform Launcher;
-    public float speed = 2f;
+    public float speed = 7f;
+    public float setTime = 2f;
+    public float distance = 1f;
 
-    private float distance = 3f;
     private Rigidbody2D projectileInstance;
+    private float currentTime;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTime = -2;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
-        {
-            Vector3 spawnPos = Launcher.transform.position + Launcher.transform.up * distance;
+            if ((Time.time - currentTime) > setTime)
+            {
+                Vector3 spawnPos = transform.position + transform.up * distance;
 
-            projectileInstance = Instantiate(projectile, spawnPos, Launcher.rotation) as Rigidbody2D;
+                projectileInstance = Instantiate(projectile, spawnPos, transform.rotation) as Rigidbody2D;
 
-            BulletMovement.speed = speed;
-        }
+                BulletMovement.speed = speed;
 
-       
+                currentTime = Time.time;
+            }
     }
 }
