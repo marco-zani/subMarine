5adf2a705a113992e633b14caec8994ec60c4020
Marco <marco.zani@marconirovereto.it>
2020-02-10 21:35:33 +0100

    added laser of death

--- subMarine/Assets/Scripts/Enemy_spawner.cs
+++ subMarine/Assets/Scripts/Enemy_spawner.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class Enemy_spawner : MonoBehaviour
 {
     public Transform player;
-    public float Xoffset, Yoffset, spawnSpace;
+    public float Xoffset, Yoffset, spawnSpace, deathZone;
     public GameObject enemyPrefab;
+    public int maxEnemy, enemyCount;
 
-    public int enemyCount = 0;
+    private EnemyController[] enemyList;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +20,7 @@ public class Enemy_spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (enemyCount < 5)
+        if (enemyCount < maxEnemy)
         {
             float x, y;
             x = Random.Range(player.position.x - Xoffset - spawnSpace, player.position.x + Xoffset + spawnSpace);
@@ -31,28 +32,49 @@ public class Enemy_spawner : MonoBehaviour
                 enemyCount++;
 
             }
+        }
 
+        enemyList = FindObjectsOfType<EnemyController>();
+        Debug.Log(enemyList.Length);
 
+        foreach (var enemy in enemyList)
+        {
 
+            if (enemy.transform.position.x < player.position.x - Xoffset - spawnSpace - deathZone || enemy.transform.position.x > player.position.x + Xoffset + spawnSpace + deathZone ||
+                enemy.transform.position.y < player.position.y - Yoffset - spawnSpace - deathZone || enemy.transform.position.y > player.position.y + Yoffset + spawnSpace + deathZone)
+            {
+                Destroy(enemy.gameObject);
+                enemyCount--;
+            }
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.green;
+        Gizmos.color = Color.blue;
 
         Gizmos.DrawLine(new Vector2(player.position.x + Xoffset, player.position.y + Yoffset), new Vector2(player.position.x + Xoffset, player.position.y - Yoffset));
         Gizmos.DrawLine(new Vector2(player.position.x + Xoffset, player.position.y - Yoffset), new Vector2(player.position.x - Xoffset, player.position.y - Yoffset));
         Gizmos.DrawLine(new Vector2(player.position.x - Xoffset, player.position.y - Yoffset), new Vector2(player.position.x - Xoffset, player.position.y + Yoffset));
         Gizmos.DrawLine(new Vector2(player.position.x - Xoffset, player.position.y + Yoffset), new Vector2(player.position.x + Xoffset, player.position.y + Yoffset));
 
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.green;
 
         Gizmos.DrawLine(new Vector2(player.position.x + Xoffset + spawnSpace, player.position.y + Yoffset + spawnSpace), new Vector2(player.position.x + Xoffset + spawnSpace, player.position.y - Yoffset - spawnSpace));
         Gizmos.DrawLine(new Vector2(player.position.x + Xoffset + spawnSpace, player.position.y - Yoffset - spawnSpace), new Vector2(player.position.x - Xoffset - spawnSpace, player.position.y - Yoffset - spawnSpace));
         Gizmos.DrawLine(new Vector2(player.position.x - Xoffset - spawnSpace, player.position.y - Yoffset - spawnSpace), new Vector2(player.position.x - Xoffset - spawnSpace, player.position.y + Yoffset + spawnSpace));
         Gizmos.DrawLine(new Vector2(player.position.x - Xoffset - spawnSpace, player.position.y + Yoffset + spawnSpace), new Vector2(player.position.x + Xoffset + spawnSpace, player.position.y + Yoffset + spawnSpace));
 
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(new Vector2(player.position.x + Xoffset + spawnSpace + deathZone, player.position.y + Yoffset + spawnSpace + deathZone),
+            new Vector2(player.position.x + Xoffset + spawnSpace + deathZone, player.position.y - Yoffset - spawnSpace - deathZone));
+        Gizmos.DrawLine(new Vector2(player.position.x + Xoffset + spawnSpace + deathZone, player.position.y - Yoffset - spawnSpace - deathZone),
+            new Vector2(player.position.x - Xoffset - spawnSpace - deathZone, player.position.y - Yoffset - spawnSpace - deathZone));
+        Gizmos.DrawLine(new Vector2(player.position.x - Xoffset - spawnSpace - deathZone, player.position.y - Yoffset - spawnSpace - deathZone),
+            new Vector2(player.position.x - Xoffset - spawnSpace - deathZone, player.position.y + Yoffset + spawnSpace + deathZone));
+        Gizmos.DrawLine(new Vector2(player.position.x - Xoffset - spawnSpace - deathZone, player.position.y + Yoffset + spawnSpace + deathZone),
+            new Vector2(player.position.x + Xoffset + spawnSpace + deathZone, player.position.y + Yoffset + spawnSpace + deathZone));
 
     }
 }
