8be6e0f41779952264d55c3a944944877567664c
Giulio <giulio.cappelli1e@gmail.com>
2020-02-10 21:41:56 +0100

    Minimap

---
+++ subMarine/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float maxHealth = 100;
+    public float currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.setMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Da togliere
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            TakeDamage(25f);
+        }
+    }
+
+    private void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+    }
+}
