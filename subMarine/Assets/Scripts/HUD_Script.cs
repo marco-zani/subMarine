662c113af7cbe5a7d7c6000097f477d96b444f4c
Marco <marco.zani@marconirovereto.it>
2020-02-13 22:05:36 +0100

    hud update

---
+++ subMarine/Assets/Scripts/HUD_Script.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HUD_Script : MonoBehaviour
+{
+    public GameObject enemyHUD;
+
+    private GameObject popUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popUp = Instantiate(enemyHUD) as GameObject;
+        HideHUD();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ShowEnemyData(string name, float life, float maxlife, string level)
+    {
+        
+
+        popUp.GetComponent<CanvasGroup>().alpha = 1;
+        Slider slider = popUp.GetComponentInChildren<Slider>();
+        slider.maxValue = maxlife;
+        slider.value = life;
+        popUp.GetComponentInChildren<Text>().text = name + "   Lv: " + level;
+    }
+
+    public void HideHUD()
+    {
+        popUp.GetComponent<CanvasGroup>().alpha = 0;
+    }
+}
